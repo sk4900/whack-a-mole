bc3edb6909666d71e132e7160db641c375aef2de
Kadin Benjamin ktb1193 <ktb1193@rit.edu>
2019-04-10 11:47:09 -0400

    [1] Updated .gitignore and some server code.

--- src/server/README.txt
+++
@@ -1,1 +0,0 @@
-This file is only here so that Git will put its directory in the repository. You may delete it.
--- src/server/WAMServer.java
+++ src/server/WAMServer.java
@@ -1,4 +1,65 @@
 package server;
 
-public class WAMServer {
+import java.io.IOException;
+
+import java.lang.IllegalArgumentExcpetion;
+import java.lang.Runnable;
+import java.lang.Thread;
+
+import java.net.ServerSocket;
+
+import java.util.Scanner;
+
+/***/
+public class WAMServer implements Runnable {
+
+    /***/
+    private ServerSocket serverSocket;
+
+    /***/
+    private int[][] board;
+
+    /***/
+    private int clients;
+
+    /***/
+    private int time;
+
+    /***/
+    public WAMServer(int port, int rows, int columns, int clients, int time)
+        throws IOExcpetion, IllegalArgumentException {
+        serverSocket = new ServerSocket(port);
+        board = new int[columns][rows];
+        this.clients = clients;
+        this.time = time;
+    }
+
+    /***/
+    public void run() {  }
+
+    /***/
+    public static void main(String[] args) {
+        while (args.length < 5) {
+            System.out.println("Usage: #game-port " +
+                "#rows #columns #players #game-time");
+            Scanner in = new Scanner(System.in);
+            args = in.nextLine().split(" ");
+        }
+        int[] params = new int[args.length];
+        for (int i = 0; i < args.length ; i++) {
+            params[i] = Integer.parseInt(args[i]);
+        }
+        WAMServer server;
+        try {
+            server = new WAMServer(params[0], params[1],
+                params[2], params[3], params[4]);
+            new Thread(server).run();
+        } catch (IOException ioe) {
+            System.out.println("error starting server");
+            //send message to clients
+        } catch (IllegalArgumentException iae) {
+            System.out.println("error starting server");
+            //send message to clients
+        }
+    }
 }
