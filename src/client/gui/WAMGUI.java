ef8c1a700dce944295a514f2c202b2cc12ca2c8f
sk4900 <43387347+sk4900@users.noreply.github.com>
2019-04-10 11:19:12 -0400

    GUI grid with buttons and text area  -SK

--- src/client/gui/WAMGUI.java
+++ src/client/gui/WAMGUI.java
@@ -2,6 +2,8 @@ package client.gui;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
@@ -12,23 +14,44 @@ import static common.WAMProtocol.*;
 
 public class WAMGUI extends Application {
 
+    private List<String> params;
+
     public static void main(String[] args) {
         launch(args);
     }
     public void init(){
             Parameters parameters = getParameters();
-            List<String> params = parameters.getRaw();
+            this.params = parameters.getRaw();
 
     }
     @Override
     public void start(Stage primaryStage) {
         GridPane grid = new GridPane();
+        int rows = Integer.parseInt(this.params.get(1));
+        int columns = Integer.parseInt(this.params.get(2));
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < columns; j++){
+                Button button = new Button();
+                button.setMaxHeight(100);
+                button.setMaxWidth(100);
+                button.setMinHeight(100);
+                button.setMinWidth(100);
+                button.setText("Mole");
+                grid.add(button, j, i);
+            }
+        }
         BorderPane border = new BorderPane();
+        TextArea text = new TextArea();
+        text.setEditable(false);
+        text.setText("Welcome to Whack-A-Mole");
+
+        text.setMaxWidth(grid.getColumnCount() * 100);
+        text.setMaxHeight(100);
+        border.setBottom(text);
         border.setCenter(grid);
         Scene scene = new Scene(border);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Whack-A-Mole");
-
         primaryStage.setResizable(false);
         primaryStage.show();
     }
