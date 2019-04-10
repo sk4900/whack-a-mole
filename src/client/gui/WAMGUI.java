9f2574b8f22588f7a9396940f1cbaee176b236cb
sk4900 <43387347+sk4900@users.noreply.github.com>
2019-04-10 11:00:48 -0400

    Basic GUI setup and run configuration -SK

--- src/client/gui/WAMGUI.java
+++ src/client/gui/WAMGUI.java
@@ -1,16 +1,35 @@
 package client.gui;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+
+import java.util.List;
+
+import static common.WAMProtocol.*;
 
 public class WAMGUI extends Application {
 
     public static void main(String[] args) {
         launch(args);
     }
+    public void init(){
+            Parameters parameters = getParameters();
+            List<String> params = parameters.getRaw();
 
+    }
     @Override
     public void start(Stage primaryStage) {
+        GridPane grid = new GridPane();
+        BorderPane border = new BorderPane();
+        border.setCenter(grid);
+        Scene scene = new Scene(border);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Whack-A-Mole");
 
+        primaryStage.setResizable(false);
+        primaryStage.show();
     }
 }
